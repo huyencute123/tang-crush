6df398ee1e5ff37822de3f1f460fc658feac78c6
huyencute123 <86838401+huyencute123@users.noreply.github.com>
2021-07-02 20:45:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Cậu à!',
     introDesc: 'Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương ',
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, cậu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu cậu <3',
-    btnReply: 'Gửi cho cậu nè <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Cảm ơn cậu về nhuuwnngx điều đa làm cho tớ <3 .',
-    btnAccept: 'hông có zì <3',
-    messLink: '  https://www.facebook.com/ngochuyen.mai.98837 . ',
+   ' Mà sao cậu cứ đi nhầm đường
+   ' Lạc vào tim em lẻ loi
+    'Đằng sau chữ yêu đây là thương ',
+    'btnIntro: ''hihi',
+   ' title: ''Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    'desc: ''Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+   ' btnYes: ''Vẫn cứ là thích cậu <33',
+   ' btnNo: ''Không, cậu à :3',
+   ' question:''Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu cậu <3',
+   ' btnReply: ''Gửi cho cậu nè <3',
+   ' reply: ''Yêu thì yêu mà không yêu thì yêu <33333333',
+   ' mess: ''Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    'messDesc: ''Cảm ơn cậu về những điều đa làm cho tớ <3 .',
+   ' btnAccept: ''hông có zì <3',
+   ' messLink: ' ' https://www.facebook.com/ngochuyen.mai.98837 . ',
 
