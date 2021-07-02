1d0076ff77756cba3ce16c9e8af5c32fb1961e3a
huyencute123 <86838401+huyencute123@users.noreply.github.com>
2021-07-02 20:49:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle: 'Cậu à!',
     introDesc: 'Trái đất vốn lạ thường
-   ' Mà sao cậu cứ đi nhầm đường
+    Mà sao cậu cứ đi nhầm đường
    ' Lạc vào tim em lẻ loi
     ' Đằng sau chữ yêu đây là thương ',
     'btnIntro: ''hihi',
-   ' title: '' Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    'desc: '' Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+   ' title: ' Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    'desc: ' Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
    ' btnYes: '' Vẫn cứ là thích cậu <33',
    ' btnNo: ''Không, cậu à :3',
    ' question:''Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu cậu <3',
